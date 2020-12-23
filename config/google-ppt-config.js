d19ec474709d61485f0771b2a5ae0a83b8054c34
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-12-22 20:00:15 -0800

    moved clientID and clientSecret into a module in keys.js and marked the file in gitignore file

---
+++ config/google-ppt-config.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+// getting access to keys.js 
+const Keys = require('Keys');
+
+passport.use(
+    new GoogleStrategy({
+        // options for the google strategy
+        clientID: Keys.google.clientID,
+        clientSecret:Keys.google.clientSecret
+    }), ( ) => {
+        // ppt callback function
+    }
+)
