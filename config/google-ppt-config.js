b700e96beda7ee6274ace1c6ccc1655b5f05df38
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-12-24 17:09:23 -0800

    added routes for login/logout/google auth

--- config/google-ppt-config.js
+++ config/google-ppt-config.js
@@ -6,9 +6,10 @@ const Keys = require('Keys');
 passport.use(
     new GoogleStrategy({
         // options for the google strategy
+        callbackURL: '/auth/google/redirect',
         clientID: Keys.google.clientID,
-        clientSecret:Keys.google.clientSecret
-    }), ( ) => {
+        clientSecret: Keys.google.clientSecret
+    }, ( ) => {
         // ppt callback function
-    }
+    })
 )
---
+++ routes/auth.routs.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+
+// auth login
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// auth logout
+router.get('/logout',(req, res)=>{
+    // handle with ppt
+    res.send('logging out');
+});
+
+// auth with google
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}))
